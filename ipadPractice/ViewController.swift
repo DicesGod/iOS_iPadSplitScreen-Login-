b67810dd1d17158002cf3eb5c8bb0e8cea42320c
Minh Le <workonly140287@gmail.com>
2019-06-13 12:14:27 -0400

    Initial Commit

---
+++ ipadPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ipadPractice
+//
+//  Created by Minh Le on 2019-06-13.
+//  Copyright © 2019 Minh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
